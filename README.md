8ca6c079bf8b7bbe9cd1aeb8c5167875067f2afc
Brandon Horst <brandonhorst@gmail.com>
2015-11-09 06:07:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lacona-phrase-system-state
+Lacona Phrase group for representing various system states (to be supplemented)
---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+/** @jsx createElement */
+import {createElement, Phrase} from 'lacona-phrase'
+
+export class RunningApplication extends Phrase {
+  describe () {
+    return null
+  }
+}
+
+export class OpenWindow extends Phrase {
+  describe () {
+    return null
+  }
+}
+
+export class BrowserTab extends Phrase {
+  describe () {
+    return null
+  }
+}
+
+export class MountedVolume extends Phrase {
+  describe () {
+    return null
+  }
+}
