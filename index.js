eeaf96be15bf23701a063e205ee443908d0c7e29
Brandon Horst <brandonhorst@gmail.com>
2016-05-12 17:59:48 -0400

    Add phrases to represent elliptical phrases

--- index.js
+++ index.js
@@ -30,5 +30,69 @@ module.exports = {
   ContactCard: {
     id: 'lacona-phrases:ContactCard',
     describe: describe
+  },
+  EmailAddress: {
+    id: 'lacona-phrases:EmailAddress',
+    describe: describe
+  },
+  PhoneNumber: {
+    id: 'lacona-phrases:PhoneNumber',
+    describe: describe
+  },
+  URL: {
+    id: 'lacona-phrases:URL',
+    describe: describe
+  },
+  Integer: {
+    id: 'lacona-phrases:Integer',
+    describe: describe
+  },
+  Decimal: {
+    id: 'lacona-phrases:Decimal',
+    describe: describe
+  },
+  Ordinal: {
+    id: 'lacona-phrases:Ordinal',
+    describe: describe
+  },
+  DigitString: {
+    id: 'lacona-phrases:DigitString',
+    describe: describe
+  },
+  String: {
+    id: 'lacona-phrases:String',
+    describe: describe
+  },
+  DateTime: {
+    id: 'lacona-phrases:DateTime',
+    describe: describe
+  },
+  Date: {
+    id: 'lacona-phrases:Date',
+    describe: describe
+  },
+  Time: {
+    id: 'lacona-phrases:Time',
+    describe: describe
+  },
+  Range: {
+    id: 'lacona-phrases:Range',
+    describe: describe
+  },
+  Day: {
+    id: 'lacona-phrases:Day',
+    describe: describe
+  },
+  Duration: {
+    id: 'lacona-phrases:Duration',
+    describe: describe
+  },
+  TimeDuration: {
+    id: 'lacona-phrases:TimeDuration',
+    describe: describe
+  },
+  DateDuration: {
+    id: 'lacona-phrases:DateDuration',
+    describe: describe
   }
 }
